5e9ad1ababcc1bd548c59d9d04605625c055dfbe
Aziz Ashurov <a.o.ashurov@gmail.com>
2020-08-09 11:16:01 +0500

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php 
-echo "salom";
-?>
+<?php
+// Version
+define('VERSION', '3.0.3.6');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
